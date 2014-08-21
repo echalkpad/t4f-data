61fc5acff012f61805551acbf07d893985d7b4c7
Eric Charles <eric@aos.io>
2014-08-21 06:05:08 +0200

    Automatic message for commit of Thu Aug 21 04:05:08 UTC 2014

---
+++ cache/ehcache/src/main/java/io/datalayer/cache/ehcache/EHCache.java
@@ -0,0 +1,11 @@
+package io.datalayer.cache.ehcache;
+
+import net.sf.ehcache.Cache;
+import net.sf.ehcache.CacheManager;
+import net.sf.ehcache.Element;
+
+import org.junit.Test;
+
+public class EHCache {
+
+}
