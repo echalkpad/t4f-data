1806f438bf92abcec7b545ae41b3d2dc117f5f74
Eric Charles <eric@aos.io>
2014-08-21 13:40:15 +0200

    Automatic message for commit of Thu Aug 21 11:40:15 UTC 2014

--- structure/xsd/jaxb/src/test/java/io/aos/xsd/model/jaxb/T4FJaxbTest.java
+++ structure/xsd/jaxb/src/test/java/io/aos/xsd/model/jaxb/T4FJaxbTest.java
@@ -16,7 +16,11 @@
  * specific language governing permissions and limitations      *
  * under the License.                                           *
  ****************************************************************/
-package io.datalayer.xsd.model.jaxb;
+package io.aos.xsd.model.jaxb;
+
+import io.aos.xsd.model.jaxb.Items;
+import io.aos.xsd.model.jaxb.ObjectFactory;
+import io.aos.xsd.model.jaxb.PurchaseOrderType;
 
 import java.io.InputStream;
 
